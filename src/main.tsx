6e7d589c6586a7e7cf66a77af93c68262cd2caf5
Luca Barone <baro.luc@gmail.com>
2021-07-05 22:44:57 +0200

    :construction: auth0 wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import createAuth0Client from '@auth0/auth0-spa-js'
 
 import {App} from './modules/App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+const main = async () => {
+  const auth0Client = await createAuth0Client({
+    domain: '',
+    client_id: '',
+  })
+  console.log(auth0Client)
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+  )
+}
+
+main()
